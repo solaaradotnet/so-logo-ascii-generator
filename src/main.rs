4bf0283ee54402a760819c995f758d57ae6b4144
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-08-03 00:59:00 +0200

    add option to insert copyright text :3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::{cmp::max, collections::btree_set::Difference};
+
+use chrono::Datelike;
 use clap::Parser;
 use figrs::{Figlet, FigletOptions};
 
@@ -16,13 +19,15 @@ const SOLAARA_LOGO_BOTTOM_HALF: &str = "        +@.
       .+%::#@+.   
     .*@@-   .:+%@.
    .*%:        ##.
-              .@@.
-              :@% 
-";
+              .@@.";
+const SOLAARA_LOGO_LAST_ROW: &str = "              :@% ";
 
 #[derive(Debug, Parser)]
 struct CliArgs {
     pub text: String,
+
+    #[arg(short = 'c', long, default_value = "false")]
+    pub with_copyright: bool,
 }
 
 fn main() {
@@ -35,13 +40,34 @@ fn main() {
     let figure = Figlet::text(args.text.to_owned(), opt).unwrap().text;
 
     let mut figure_lines = figure.lines();
-    let new_bottom_half: Vec<String> = SOLAARA_LOGO_BOTTOM_HALF
+    let (new_bottom_half, max_width) = SOLAARA_LOGO_BOTTOM_HALF
         .lines()
         .map(|logo_row| logo_row.to_owned() + (figure_lines.next().unwrap_or("")))
-        .collect();
-    let new_bottom_half = new_bottom_half.join("\n");
+        .fold((String::new(), 0), |mut acc, el| {
+            acc.0.push_str(&el);
+            acc.0.push('\n');
+            acc.1 = max(acc.1, el.len());
+            acc
+        });
 
-    let finished_thing = SOLAARA_LOGO_TOP_HALF.to_owned() + &new_bottom_half;
+    let last_row = if args.with_copyright {
+        let current_time = chrono::Local::now();
+        let current_year = current_time.year();
+        let copyright_string = format!("(C) Solaara's Network {}", current_year);
 
+        SOLAARA_LOGO_LAST_ROW.to_owned()
+            + &" ".repeat(
+                if max_width >= (SOLAARA_LOGO_LAST_ROW.len() + copyright_string.len()) {
+                    max_width - SOLAARA_LOGO_LAST_ROW.len() - copyright_string.len()
+                } else {
+                    4
+                },
+            )
+            + &copyright_string
+    } else {
+        SOLAARA_LOGO_LAST_ROW.to_owned()
+    };
+
+    let finished_thing = SOLAARA_LOGO_TOP_HALF.to_owned() + &new_bottom_half + &last_row;
     println!("{finished_thing}")
 }
